f7233bd6c72dbeecc5cdce983506e6b734dac15f
Jose Tristani <jose.tristani@estadisticas.pr>
2016-12-05 09:58:09 -0400

    Final version Compara tu negocio.

--- src/Controller/ComparaTuNegocioController.php
+++ src/Controller/ComparaTuNegocioController.php
@@ -1,15 +1,38 @@
-namespace Drupal\ComparaTuNegocio\Controller;
+<?php
+
+namespace Drupal\compara_tu_negocio\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 class ComparaTuNegocioController extends ControllerBase {
 
   public function index() {
-    $message = $this->t('%from to %to', [
-      '%from' => $from,
-      '%to' => $to,
-    ]);
+    $message = 'Hello World';
 
-    return ['#markup' => $message];
+    $query = \Drupal::database()->select('compara_tu_negocio', 'ctn');
+    $query->addField('ctn','naics_code');
+    $query->addField('ctn','code_description');
+
+    $query->addField('ctn','num_establishments_2002');
+    $query->addField('ctn','value_x1000_2002');
+    $query->addField('ctn','payroll_x1000_2002');
+    $query->addField('ctn','num_employees_2002');
+
+    $query->addField('ctn','num_establishments_2007');
+    $query->addField('ctn','value_x1000_2007');
+    $query->addField('ctn','payroll_x1000_2007');
+    $query->addField('ctn','num_employees_2007');
+
+    $query->addField('ctn','num_establishments_2012');
+    $query->addField('ctn','value_x1000_2012');
+    $query->addField('ctn','payroll_x1000_2012');
+    $query->addField('ctn','num_employees_2012');
+    $results = $query->execute()->fetchAll();
+    //kint($results[1]);
+    //kint($results);
+    return array(
+    	'#theme' => 'compara_tu_negocio',
+    	'#ctn_data' => $results,
+    	);
   }
 }
